f0a137f1ec1e7874153cd5c7eed7fccdc7e1f48a
github_dominikus02-source <dominikus.02@gmail.com>
2026-06-24 10:34:53 +0700

    feat: replace OnboardingWizard with simplified version

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { OnboardingWizard } from '@/components/onboarding/OnboardingWizard'
+import OnboardingWizard from '@/components/onboarding/OnboardingWizard'
 
 export default function OnboardingPage() {
   return <OnboardingWizard />
